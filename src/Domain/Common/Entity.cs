3848f40fdef996331f4b0632283edf9c44854c73
Roy Sand <roy@sanddata.no>
2025-11-12 21:36:52 +0100

    Added Enity and Valueobject base class

---
+++ src/Domain/Common/Entity.cs
@@ -0,0 +1,40 @@
+﻿namespace Domain.Common;
+
+public abstract class Entity(Guid id)
+{
+    private Guid Id { get; } = id;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (GetType() != obj.GetType())
+            return false;
+
+        var entity = (Entity)obj;
+
+        return Id.Equals(entity.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity a, Entity b)
+    {
+        if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            return true;
+
+        if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            return false;
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity a, Entity b)
+    {
+        return !(a == b);
+    }
+}
